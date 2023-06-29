a0b883c1fa89424d42b3ebf6d885998c22b82842
Aadhav Rajesh <aadhav.rajesh@gmail.com>
2023-06-29 17:43:17 +0530

    Update spam.py

--- spam.py
+++ spam.py
@@ -3,6 +3,7 @@ import pyautogui
 
 def spamscript():
     time.sleep(5)
+    # this is a delay so you can switch to your desired target application
     with open('script.txt', encoding="utf-8") as d:
         lines = d.readlines()
     for i in lines:
